32132a86fb048fc2087b1618089c92395cdd033b
pachumon <shenoy.prasanth@gmail.com>
2024-04-16 10:10:40 +0000

    handling exception throwing and catching exception and logging details

--- camel-xmldsl-app_working/src/main/java/com/pachumon/FileProcessor.java
+++ camel-xmldsl-app_working/src/main/java/com/pachumon/FileProcessor.java
@@ -5,18 +5,23 @@ package com.pachumon;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 
  */
 public class FileProcessor implements Processor {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileProcessor.class);
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		// TODO Auto-generated method stub
-		String originalFileContent = (String) exchange.getIn().getBody(String.class);
-        String upperCaseFileContent = originalFileContent.toUpperCase();
-        exchange.getIn().setBody(upperCaseFileContent);
+		LOGGER.info("entering the processor flow");
+		throw new IllegalArgumentException("An exception happened on purpose");
+//		String originalFileContent = (String) exchange.getIn().getBody(String.class);
+//        String upperCaseFileContent = originalFileContent.toUpperCase();
+//        exchange.getIn().setBody(upperCaseFileContent);
 	}
 
 }
